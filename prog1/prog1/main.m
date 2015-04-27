2a37585583c8178405d87df25ca8ed9fb7224b9f
OA Wu <comdan66@gmail.com>
2015-04-27 20:31:19 +0800

    First code.

---
+++ prog1/prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by OA Wu on 2015/4/27.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
