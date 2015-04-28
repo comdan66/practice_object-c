f1e34d7603ab7e50743e01add6a19b6fec008f9f
OA Wu <comdan66@gmail.com>
2015-04-28 19:25:33 +0800

    Sample 6.2

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -8,15 +8,58 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+
+-(void) print;
+-(void) setNumerator;
+-(void) setDenominator;
+-(int) numerator;
+-(int) denominator;
+-(double) covertToNum;
+
+@end
+
+@implementation Fraction {
+    int numerator;
+    int denominator;
+}
+
+-(void) print {
+    NSLog(@" %i / %i", numerator, denominator);
+}
+-(void) setNumerator: (int) n {
+    numerator = n;
+}
+-(void) setDenominator: (int) d {
+    denominator = d;
+}
+-(int) numerator {
+    return numerator;
+}
+-(int) denominator {
+    return denominator;
+}
+-(double) covertToNum {
+    if (denominator != 0)
+        return (double) numerator / denominator;
+    else
+        return NAN;
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        int integer = 0;
+        Fraction *frac1 = [[Fraction alloc] init];
+        Fraction *frac2 = [[Fraction alloc] init];
         
-        NSLog(@"input: ");
-        scanf("%i", &integer);
+        [frac1 setNumerator: 1];
+        [frac1 setDenominator: 4];
+        [frac1 print];
+        NSLog(@" = %g", [frac1 covertToNum]);
         
-        NSLog(@"val: %i", integer);
-    }
+        NSLog(@" = %g", [frac2 covertToNum]);
+        
+   }
     return 0;
 }
