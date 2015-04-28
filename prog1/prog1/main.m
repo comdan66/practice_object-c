a3623c6dfeb19d254204bae1358f9e1cc94ff215
OA Wu <comdan66@gmail.com>
2015-04-28 13:15:10 +0800

    Sample 4.2

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -11,16 +11,25 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        int integerVar = 100;
-        float floatingVar = 331.79;
-        double doubleVar = 8.44e+11;
-        char charVar = 'w';
+        int a = 100;
+        int b = 2;
+        int c = 25;
+        int d = 4;
+        int result;
         
-        NSLog(@"integer value = %i", integerVar);
-        NSLog(@"float value = %f", floatingVar);
-        NSLog(@"double value = %e", doubleVar);
-        NSLog(@"double value = %g", doubleVar);
-        NSLog(@"char value = %c", charVar);
+        result = a - b;
+        NSLog(@"a - b = %i", result);
+        
+        result = b * c;
+        NSLog(@"b * c = %i", result);
+        
+        result = a / c;
+        NSLog(@"a / c = %i", result);
+        
+        result = a + b * c;
+        NSLog(@"a + b * c = %i", result);
+        
+        NSLog(@"a * b + c * d = %i", a * b + c * d);
     }
     return 0;
 }
