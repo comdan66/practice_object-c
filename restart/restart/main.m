ec24e221d6fac27cb8a904eb6523275e2953404b
OA Wu <comdan66@gmail.com>
2015-06-10 12:10:49 +0800

    Restart

---
+++ restart/restart/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  restart
+//
+//  Created by OA Wu on 2015/6/10.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
