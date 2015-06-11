570baf97011fa8dd91f69f0321d06984ebc6435d
OA Wu <comdan66@gmail.com>
2015-06-11 22:12:48 +0800

    GoNextView

---
+++ GoNextView/GoNextView/MyViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyViewController.h
+//  GoNextView
+//
+//  Created by OA Wu on 2015/6/11.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
