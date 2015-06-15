8df61e3af1c7b1ba44acbab0ded3e5dd7e490021
OA Wu <comdan66@gmail.com>
2015-06-15 20:00:30 +0800

    practice add table6

---
+++ Table6/Table6/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  Table6
+//
+//  Created by OA Wu on 2015/6/15.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
---
+++ Table6/Table6/MyTableViewController.m
@@ -0,0 +1,117 @@
+//
+//  MyTableViewController.m
+//  Table6
+//
+//  Created by OA Wu on 2015/6/15.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import "MyTableViewController.h"
+
+@interface MyTableViewController () {
+    NSMutableArray *list;
+}
+
+@end
+
+@implementation MyTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    list = [NSMutableArray new];
+    
+    self.refreshControl = [UIRefreshControl new];
+    [self.refreshControl addTarget:self action:@selector(headleRefresh) forControlEvents:UIControlEventValueChanged];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(void) viewDidAppear:(BOOL)animated {
+    self.refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"更新中.."];
+}
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [list count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+
+    
+    if (cell == nil) {
+        cell = [UITableViewCell new];
+    }
+    
+    cell.textLabel.text = [list objectAtIndex:indexPath.row];
+//    cell.textLabel.text = [NSString stringWithFormat:@"%d", ++i];
+    
+    return cell;
+}
+-(void)headleRefresh {
+    static int i = 0;
+    [list addObject:[NSString stringWithFormat:@"%d", ++i]];
+    
+//    [self.refreshControl endRefreshing];
+    [self.tableView reloadData];
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
