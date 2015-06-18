bed2de29b135912fe313590f719a7deec9129541
OA Wu <comdan66@gmail.com>
2015-06-18 21:19:02 +0800

    temp.

---
+++ Table8/Table8/MyTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.m
+//  Table8
+//
+//  Created by OA Wu on 2015/6/18.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import "MyTableViewCell.h"
+
+@implementation MyTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Table8/Table8/MyTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyTableViewController.h
+//  Table8
+//
+//  Created by OA Wu on 2015/6/18.
+//  Copyright (c) 2015年 OA Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MyTableViewCell.h"
+
+@interface MyTableViewController : UITableViewController
+
+@end
